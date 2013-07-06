5c5f897b9bcec8f123041e0cc8d35a800f04640b
Brecht Devleesschauwer <Brecht.Devleesschauwer@UGent.be>
2013-07-06 18:32:47 +0200

    delete folder with plots

--- server.R
+++ server.R
@@ -3,10 +3,18 @@ shinyServer(function(input, output) {
   output$downloadPDF <-
     downloadHandler(filename = "report.pdf",
                     content = function(file){
+                      # generate PDF
                       knit2pdf("report.Rnw")
+
+                      # copy pdf to 'file'
                       file.copy("report.pdf", file)
+
+                      # delete generated files
                       file.remove("report.pdf", "report.tex",
                                   "report.aux", "report.log")
+
+                      # delete folder with plots
+                      unlink("figure", recursive = TRUE)
                     },
                     contentType = "application/pdf"
   )
@@ -14,9 +22,17 @@ shinyServer(function(input, output) {
   output$downloadHTML <-
     downloadHandler(filename = "report.html",
                     content = function(file){
+                      # generate HTML
                       knit2html("report.Rmd")
+                      
+                      # copy html to 'file'
                       file.copy("report.html", file)
+                      
+                      # delete generated files
                       file.remove("report.html", "report.md")
+
+                      # delete folder with plots
+                      unlink("figure", recursive = TRUE)
                     },
                     contentType = "text/html"
   )
